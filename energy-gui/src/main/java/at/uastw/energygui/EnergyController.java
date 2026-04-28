b9cfd33159d400bbb7f6e52aced7a264ef78a6d7
Markus <wi24b012@technikum-wien.atm>
2026-04-28 14:01:24 +0200

    "Add time filter for historical data"

--- energy-gui/src/main/java/at/uastw/energygui/EnergyController.java
+++ energy-gui/src/main/java/at/uastw/energygui/EnergyController.java
@@ -1,6 +1,8 @@
 package at.uastw.energygui;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 
@@ -8,6 +10,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EnergyController {
 
@@ -15,6 +19,20 @@ public class EnergyController {
     @FXML private ListView<String> lv_historical;
     @FXML private DatePicker dp_start;
     @FXML private DatePicker dp_end;
+    @FXML private ComboBox<String> cb_start_hour;
+    @FXML private ComboBox<String> cb_end_hour;
+
+    @FXML
+    public void initialize() {
+        List<String> hours = new ArrayList<>();
+        for (int i = 0; i < 24; i++) {
+            hours.add(String.format("%02d:00", i));
+        }
+        cb_start_hour.setItems(FXCollections.observableArrayList(hours));
+        cb_end_hour.setItems(FXCollections.observableArrayList(hours));
+        cb_start_hour.setValue("00:00");
+        cb_end_hour.setValue("23:00");
+    }
 
     @FXML
     protected void onRefreshClicked() throws Exception {
@@ -24,23 +42,21 @@ public class EnergyController {
                 .GET().build();
 
         String body = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
-
         lv_current.getItems().clear();
         lv_current.getItems().add(body);
     }
 
     @FXML
     protected void onLoadHistoricalClicked() throws Exception {
-        String start = dp_start.getValue().toString();
-        String end = dp_end.getValue().toString();
+        String start = dp_start.getValue().toString() + "T" + cb_start_hour.getValue() + ":00";
+        String end = dp_end.getValue().toString() + "T" + cb_end_hour.getValue() + ":00";
 
         HttpClient client = HttpClient.newBuilder().build();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/energy/historical?start=" + start + "T00:00:00&end=" + end + "T23:59:59"))
+                .uri(URI.create("http://localhost:8080/energy/historical?start=" + start + "&end=" + end))
                 .GET().build();
 
         String body = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
-
         lv_historical.getItems().clear();
         lv_historical.getItems().add(body);
     }
